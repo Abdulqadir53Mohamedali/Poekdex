16fe8d34e4a0faae0092e62609673ffb9e1340d2
Abdulqadir53Mohamedali <abdulqadir.tm@gmail.com>
2023-10-01 20:24:01 +0100

    big changes

--- pokedex/scripts/api.js
+++ pokedex/scripts/api.js
@@ -19,12 +19,16 @@ btnSearch.addEventListener("click", () =>{
 
         const name = document.querySelector("#name")
         name.textContent = res.name.toUpperCase();
-        const height = document.querySelector("#height")
-        height.textContent = `${res.height}m`;
-        const weight = document.querySelector("#weight")
-        weight.textContent = `${res.weight}kg`;
+        name.style.fontSize = "20px";
 
+        const height = document.querySelector("#height");
+        height.innerHTML = `Height: ${res.height}m <br>`;
+        
+        const weight = document.querySelector("#weight");
+        weight.innerHTML = `Weight: ${res.weight}kg`;
+        
         const types = document.querySelector("#types");
+        const ability = document.querySelector("#ability");
         types.innerHTML = "";
 
         for (let index = 0; index < res.types.length; index++) {
@@ -33,18 +37,55 @@ btnSearch.addEventListener("click", () =>{
         }
         
         const stats = document.querySelector("#stats");
-        stats.innerHTML = "";
-
+        stats.innerHTML = "<b>Stats:<b><br>";
         for (let index = 0; index < res.stats.length; index++) {
+          let name = res.stats[index].stat.name.toUpperCase();
+          let percentage = (res.stats[index].base_stat / 450) * 100;
+      
+          let barColorClass = "";
+          switch (name) {
+              case "HP":
+                  barColorClass = "bg-danger";
+                  break;
+              case "ATTACK":
+                  barColorClass = "bg-warning";
+                  break;
+              case "DEFENSE":
+                  barColorClass = "bg-success";
+                  break;
+              case "SPECIAL ATTACK":
+                  barColorClass = "bg-primary";
+                  break;
+              case "SPECIAL DEFENSE":
+                  barColorClass = "bg-info";
+                  break;
+              case "SPEED":
+                  barColorClass = "bg-info";
+                  break;
+              default:
+                  break;
+          }
+      
+          stats.innerHTML += `
+              <div>${name}</div>
+              <div class="progress" role="progressbar" aria-label="${name} progress bar" aria-valuenow="${res.stats[index].base_stat}" aria-valuemin="0" aria-valuemax="100" style="height: 20px">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated ${barColorClass}" style="width: ${percentage}%">${res.stats[index].base_stat}</div>
+              </div>`;
+      }
+      
+      
 
-            let name = res.stats[index].stat.name.toUpperCase();
-            let percentage = (res.stats[index].base_stat / 450) * 100
-            
-            stats.innerHTML += `<div>${name}</div>
-          <div class="progress" role="progressbar" aria-label="Example 20px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="height: 20px">
-            <div class="progress-bar" style="width: ${percentage}%">${res.stats[index].base_stat}</div>
-          </div>` 
+
+
+        ability.innerHTML = "<b>Abilities:<b><br>";
+        
+        for (let index = 0; index < res.abilities.length; index++) {
+          let abilityName = res.abilities[index].ability.name.toUpperCase();
+          ability.innerHTML += `<span>${abilityName}</span><br> `;
         }
+
+
+
         
     })
 
